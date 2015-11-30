655f1089c1c612d8f7706a458eb7601851162648
damien75 <damien75@users.noreply.github.com>
2015-11-29 21:36:11 -0500

    Create cutTheTree.py

---
+++ cutTheTree.py
@@ -0,0 +1,47 @@
+#source Hackerrank / Algorithms / Search
+
+#input: tree with weights
+
+#goal: cut in 2 trees with minimal total weight difference
+
+#idea: keep a dict of the connected nodes and a list of the weights
+#Using DFS with a set of visited nodes and a stack of nodes to visit,
+#Add the weight of the current subtree to the parent so that sutTotals gives the weight of the subtree when we move up
+#subTotals : node => weight of its subtree with node as root
+#and the best difference so far
+
+def minDiffCut(graph, weights , vertex):
+    total = sum(weights)
+    visited = set()
+    bestDiff = total
+    toVisit = [vertex]
+    toVisitSet = set()
+    toVisitSet.add(vertex)
+    subTotals = {vertex : weights[vertex] for vertex in graph.keys()}
+    while len(toVisit) > 0:
+        currVertex = toVisit[-1]
+        hasChildrenNotVisited = False
+        for next_vtx in graph[currVertex]:
+            if next_vtx in visited or next_vtx in toVisitSet: #we need to check if it is in the list to visit but is not yet considered visited, even if it's in the list
+                continue
+            toVisit.append(next_vtx)
+            toVisitSet.add(next_vtx)
+            hasChildrenNotVisited = True
+        if not hasChildrenNotVisited:
+            visited.add(currVertex)
+            toVisit.pop() #we have visited all its children, we can now remove this node from the list to visit
+            for next_vtx in graph[currVertex]:
+                if next_vtx not in visited:
+                    subTotals[next_vtx] += subTotals[currVertex] #we update the weight of its parent
+            bestDiff = min(bestDiff, abs(2*subTotals[currVertex] - total))
+    return bestDiff
+
+nbNodes = input()
+weights = map(int , raw_input().split())
+tree = {i : [] for i in range(nbNodes)}
+for _ in range(nbNodes - 1):
+    parent , child = map(int , raw_input().split())
+    tree[parent - 1].append(child - 1)
+    tree[child - 1].append(parent - 1)
+
+print minDiffCut(tree , weights , 0)
