ef3a604d7ca7321849a32661d0ebab8a9cc548f9
Damien Goblot <damiengoblot@gmail.com>
2015-12-04 21:01:29 -0500

    Speed improvement removing if else statement

--- floydWarshall.py
+++ floydWarshall.py
@@ -4,7 +4,11 @@
 
 #goal: compute shortest path between 2 nodes for q queries
 
-#idea
+#idea: HAVE A 2D array and not 3D!!!
+#array A that keeps track of shortest length from i to j
+#we will update this array by adding vertices 1 by 1
+#At the beginning, A[i][j] = None if no edge i -> j, 0 if i == j , cost if there is an edge
+#Then for k = 1..n, A[i][j] = min(A[i][j] , A[i][k] + A[k][j])
 
 """Tricks to improve python speed:
 1) Use list of lists instead if dictionary.
@@ -24,43 +28,7 @@ class Floyd:
     def __init__(self , graph):
         self.graph = graph
 
-    def shortestDistance2(self):
-        n = len(self.graph)
-        A = [[[None for _ in range(n + 1)] for _ in range(n)] for _ in range(n)]
-        for i in range(n):
-            v1 = self.graph[i]
-            A1 = A[i]
-            for j in range(n):
-                if i == j:
-                    A1[j][0] = 0
-                elif v1[j] is not None:
-                    A1[j][0] = v1[j]
-
-        for k in range(1 , n + 1):
-            v1 = A[k - 1]
-            for i in range(n):
-                v2 = A[i]
-                cost1 = v2[k - 1][k - 1]
-                for j in range(n):
-                    if v1[j][k - 1] is None or cost1 is None:
-                        v2[j][k] = v2[j][k - 1]
-                        continue
-                    v3 = v2[j]
-                    cost2 = v3[k - 1]
-                    cost3 = v1[j][k - 1]
-                    b1 = cost2 is not None
-                    b2 = cost1 is not None and cost3 is not None
-                    if b1 and b2:
-                        v3[k] = min(cost2 , cost1 + cost3)
-                    elif b2:
-                        v3[k] = cost1 + cost3
-        self.A = A
-
     def shortestDistance(self):
-        """print '------------------------------------------'
-        print '------------------------------------------'
-        print '------------------------------------------'"""
-
         n = len(self.graph)
         A = [[None for _ in range(n)] for _ in range(n)]
         for i in range(n):
@@ -71,40 +39,19 @@ class Floyd:
                     A1[j] = 0
                 elif v1[j] is not None:
                     A1[j] = v1[j]
-        """print A
-        print"""
         for k in range(n):
             middle = A[k]
-            """print
-            print 'k' , k , middle"""
             for i in range(n):
                 fromVertex = A[i]
-                """print
-                print 'i' , i , fromVertex[k]"""
-                """if fromVertex[k][k] is None: #then impossible to go from vertex i to middle vertex k
-                    print 'none 1'
-                    continue"""
                 cost1 = fromVertex[k] #best cost to go from initial vertex to middle vertex
-                if cost1 is None:
+                if cost1 is None: #then impossible to go from vertex i to middle vertex k
                     continue
                 for j in range(n):
                     cost3 = middle[j] #best cost to go from middle vertex to end vertex
-                    """print 'j' , j , middle[j]"""
                     if cost3 is None: #impossible to go from middle vertex to end vertex
-                        """print 'none 2'"""
                         continue
-                    cost2 = fromVertex[j] # best path cost from initial to end vertex so far without k
-                    """print i , k , j , cost1 , cost3 , cost2"""
-                    if cost2 is not None:
-                        fromVertex[j] = min(cost2 , cost1 + cost3)
-                    else:
-                        fromVertex[j] = cost1 + cost3
-                    """elif b1:
-                        v3[k + 1:] = cost2"""
-                    """print A"""
+                    fromVertex[j] = min(fromVertex[j] , cost1 + cost3) or cost1 + cost3
 
-        #print len(A) , len(A[0]) , len(A[0][0])
-        """print A"""
         self.A = A
 
     def shortestDistanceBetween(self , fromVertex , toVertex):
