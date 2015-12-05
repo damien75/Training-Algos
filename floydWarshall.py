b68ef9ea4d5cbdac1e4a8ba8647024e1a1def704
Damien Goblot <damiengoblot@gmail.com>
2015-12-04 19:59:09 -0500

    Huge improvement by removing 3rd dimension of array A. We don't need to store all values for 0..k, just the last one

--- floydWarshall.py
+++ floydWarshall.py
@@ -24,7 +24,7 @@ class Floyd:
     def __init__(self , graph):
         self.graph = graph
 
-    def shortestDistance(self):
+    def shortestDistance2(self):
         n = len(self.graph)
         A = [[[None for _ in range(n + 1)] for _ in range(n)] for _ in range(n)]
         for i in range(n):
@@ -40,11 +40,10 @@ class Floyd:
             v1 = A[k - 1]
             for i in range(n):
                 v2 = A[i]
-                if v2[k - 1] is None:
-                    continue
                 cost1 = v2[k - 1][k - 1]
                 for j in range(n):
-                    if v1[j] is None:
+                    if v1[j][k - 1] is None or cost1 is None:
+                        v2[j][k] = v2[j][k - 1]
                         continue
                     v3 = v2[j]
                     cost2 = v3[k - 1]
@@ -53,31 +52,77 @@ class Floyd:
                     b2 = cost1 is not None and cost3 is not None
                     if b1 and b2:
                         v3[k] = min(cost2 , cost1 + cost3)
-                    elif b1:
-                        v3[k] = cost2
                     elif b2:
                         v3[k] = cost1 + cost3
+        self.A = A
+
+    def shortestDistance(self):
+        """print '------------------------------------------'
+        print '------------------------------------------'
+        print '------------------------------------------'"""
+
+        n = len(self.graph)
+        A = [[None for _ in range(n)] for _ in range(n)]
+        for i in range(n):
+            v1 = self.graph[i]
+            A1 = A[i]
+            for j in range(n):
+                if i == j:
+                    A1[j] = 0
+                elif v1[j] is not None:
+                    A1[j] = v1[j]
+        """print A
+        print"""
+        for k in range(n):
+            middle = A[k]
+            """print
+            print 'k' , k , middle"""
+            for i in range(n):
+                fromVertex = A[i]
+                """print
+                print 'i' , i , fromVertex[k]"""
+                """if fromVertex[k][k] is None: #then impossible to go from vertex i to middle vertex k
+                    print 'none 1'
+                    continue"""
+                cost1 = fromVertex[k] #best cost to go from initial vertex to middle vertex
+                if cost1 is None:
+                    continue
+                for j in range(n):
+                    cost3 = middle[j] #best cost to go from middle vertex to end vertex
+                    """print 'j' , j , middle[j]"""
+                    if cost3 is None: #impossible to go from middle vertex to end vertex
+                        """print 'none 2'"""
+                        continue
+                    cost2 = fromVertex[j] # best path cost from initial to end vertex so far without k
+                    """print i , k , j , cost1 , cost3 , cost2"""
+                    if cost2 is not None:
+                        fromVertex[j] = min(cost2 , cost1 + cost3)
+                    else:
+                        fromVertex[j] = cost1 + cost3
+                    """elif b1:
+                        v3[k + 1:] = cost2"""
+                    """print A"""
 
         #print len(A) , len(A[0]) , len(A[0][0])
-        #print A
+        """print A"""
         self.A = A
 
     def shortestDistanceBetween(self , fromVertex , toVertex):
-        return self.A[fromVertex][toVertex][-1]
+        return self.A[fromVertex][toVertex]
 
 #Read input from Hackerrank
 """n , m = map(int , raw_input().split())
-edges = {i : {j : None for j in range(1 , n + 1)} for i in range(1 , n + 1)}
+edges = [[None for _ in range(n)] for _ in range(n)]
 for _ in range(m):
     tail , head , weight = map(int , raw_input().split())
-    edges[tail][head] = weight
+    edges[tail - 1][head - 1] = weight
 
 flo = Floyd(edges)
 flo.shortestDistance()
 q = input()
 for _ in range(q):
     fromVertex , toVertex = map(int , raw_input().split())
-    print flo.shortestDistanceBetween(fromVertex , toVertex) or -1
+    print flo.shortestDistanceBetween(fromVertex - 1 , toVertex - 1) or -1
 """
 
 #Custom Inputs
