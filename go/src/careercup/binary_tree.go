844a87eedc184ac551ef5c50cb2f61a1e898e08d
Damien Goblot <damien.goblot@imagia.com>
2019-12-28 08:15:46 -0500

    add binary tree example and run unit tests on matrix of versions

---
+++ go/src/careercup/binary_tree.go
@@ -0,0 +1,80 @@
+package careercup
+
+import (
+	"fmt"
+	"math/rand"
+	"reflect"
+)
+
+type Tree struct {
+	Left  *Tree
+	Value int
+	Right *Tree
+}
+
+func New(k int) *Tree {
+	var t *Tree
+	for _, v := range rand.Perm(10) {
+		t = insert(t, (1+v)*k)
+	}
+	return t
+}
+
+func insert(t *Tree, v int) *Tree {
+	if t == nil {
+		return &Tree{nil, v, nil}
+	}
+	if v < t.Value {
+		t.Left = insert(t.Left, v)
+	} else {
+		t.Right = insert(t.Right, v)
+	}
+	return t
+}
+
+func (t *Tree) String() string {
+	if t == nil {
+		return "()"
+	}
+	s := ""
+	if t.Left != nil {
+		s += t.Left.String() + " "
+	}
+	s += fmt.Sprint(t.Value)
+	if t.Right != nil {
+		s += " " + t.Right.String()
+	}
+	return "(" + s + ")"
+}
+
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+func Walk(t *Tree, ch chan int, closeChannel bool) {
+	if t.Left != nil {
+		Walk(t.Left, ch, false)
+	}
+	ch <- t.Value
+	if t.Right != nil {
+		Walk(t.Right, ch, false)
+	}
+	if closeChannel {
+		close(ch)
+	}
+}
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+func Same(t1, t2 *Tree) bool {
+	var t1Nodes, t2Nodes []int
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	go Walk(t1, ch1, true)
+	for i := range ch1 {
+		t1Nodes = append(t1Nodes, i)
+	}
+	go Walk(t2, ch2, true)
+	for i := range ch2 {
+		t2Nodes = append(t2Nodes, i)
+	}
+	return reflect.DeepEqual(t1Nodes, t2Nodes)
+}
---
+++ go/src/careercup/binary_tree_test.go
@@ -0,0 +1,62 @@
+package careercup
+
+import (
+	"testing"
+)
+
+// https://tour.golang.org/concurrency/7
+func TestWalkTree(t *testing.T) {
+	tree := Tree{
+		Left: &Tree{
+			Left: &Tree{
+				Left:  nil,
+				Value: 1,
+				Right: nil,
+			},
+			Value: 1,
+			Right: &Tree{
+				Left:  nil,
+				Value: 2,
+				Right: nil,
+			},
+		},
+		Value: 3,
+		Right: &Tree{
+			Left: &Tree{
+				Left:  nil,
+				Value: 5,
+				Right: nil,
+			},
+			Value: 8,
+			Right: &Tree{
+				Left:  nil,
+				Value: 13,
+				Right: nil,
+			},
+		},
+	}
+	c := make(chan int)
+	expectedNodes := []int{1, 1, 2, 3, 5, 8, 13}
+	currentIndex := 0
+	go Walk(&tree, c, true)
+	for i := range c {
+		if i != expectedNodes[currentIndex] {
+			t.Errorf("Expected side view of tree to have node %d for index %d, got %d", expectedNodes[currentIndex], currentIndex, i)
+		}
+		currentIndex += 1
+	}
+}
+
+func TestConstructSameSizeSideViewEqual(t *testing.T) {
+	tree1, tree2 := New(1), New(1)
+	if !Same(tree1, tree2) {
+		t.Errorf("Expected to have the same side view for 2 ordered binary tree with same node values, got %s != %s", tree1, tree2)
+	}
+}
+
+func TestConstructDifferentSizesSideViewNotEqual(t *testing.T) {
+	tree1, tree2 := New(1), New(2)
+	if Same(tree1, tree2) {
+		t.Errorf("Expected to have different side view for 2 ordered binary tree with different node values, got %s == %s", tree1, tree2)
+	}
+}
